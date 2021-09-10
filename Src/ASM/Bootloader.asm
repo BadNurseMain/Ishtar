aea9218a202dede73104619665679b32ba35cbdd
Chloe Moran <80408197+BadNurseMain@users.noreply.github.com>
2021-09-10 10:47:30 +0100

    Update Bootloader.asm

--- Src/ASM/Bootloader.asm
+++ Src/ASM/Bootloader.asm
@@ -3,12 +3,12 @@
 ; Setting up Data and Stack Segment Registers,
 ; Stack Segment comes after Data Segment.
 _setup:
-	mov ax, 07C0h		; Set up 4K stack space after this bootloader
+	mov ax, 0x07C0		; Set up 4K stack space after this bootloader
 	add ax, 288		; (4096 + 512) / 16 bytes per paragraph
 	mov ss, ax
 	mov sp, 4096
 
-	mov ax, 07C0h		; Set data segment to where we're loaded
+	mov ax, 0x07C0		; Set data segment to where we're loaded
 	mov ds, ax
 
 	mov ax, 0x1000		; Set up Extra Segment
