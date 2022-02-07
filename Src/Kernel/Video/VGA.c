c3e07ea8b5ec3a909fbc9643445bfa954015b228
Chloe Moran <80408197+BadNurseMain@users.noreply.github.com>
2022-02-07 04:15:52 +0000

    Create VGA.c

---
+++ Src/Kernel/Video/VGA.c
@@ -0,0 +1,36 @@
+#include "Video.h"
+
+//Memory Mapped Addresses for VGA.
+#define GFX_VGA_VIDEOMEM        0xA0000
+#define GFX_VGA_TEXTMEM         0xB8000
+#define GFX_VGA_MODE            0x0449
+
+void setVideoMode(int8_t Mode)
+{
+    int8_t* ModeAddress = GFX_VGA_MODE;
+    *ModeAddress = Mode;
+    return;
+}
+
+int8_t getVideoMode()
+{
+    int8_t* ModeAddress = GFX_VGA_MODE;
+    return *ModeAddress;
+}
+
+void placeChar(int8_t Char, int16_t Location)
+{
+    int8_t XPosition = LOWORD(Location);
+    int8_t YPosition = HIWORD(Location);
+
+    int16_t* CharAddress = GFX_VGA_TEXTMEM + (XPosition * 80 + YPosition) * 2;
+
+    *CharAddress = VGA_DEFAULT_CHAR | Char;
+    return;
+}
+
+void placePixel(int8_t Colour, int32_t Location)
+{
+    
+    return;
+}
